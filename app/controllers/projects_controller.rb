0645df2e7c18d0a0c10a72956ad8834b16683cca
Mike Wilder <mikewilder01@gmail.com>
2022-01-27 11:40:48 -0500

    Reshaped User class

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,10 +3,16 @@ class ProjectsController < ApplicationController
     def show 
         project = Project.find_by(id: params[:id])
         if project 
-            
             render json: project
         else 
            render json:  "We Couldn't Find A Project With Those Credentials"
         end 
     end 
+
+    def index 
+        projects = Project.all 
+        if projects 
+            render json: projects 
+        end
+    end
 end
